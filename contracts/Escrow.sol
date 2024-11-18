34d57f68cb6ba861e75d2e6e3d8cf6269bd609b7
wavezboy <sanusifatah16@gmail.com>
2024-11-18 15:20:34 +0100

    new changes

---
+++ contracts/Escrow.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address payable public seller;
+    address public inspector;
+    address public lender;
+    address public nftAddress;
+
+    constructor(address _nftAddress, address payable _seller, address _inspector, address _lender ){
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+
+    
+
+}
