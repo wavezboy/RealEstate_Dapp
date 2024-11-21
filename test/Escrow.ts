6302f5f88adaeee6f1f2fe0266a895d824df7146
wavezboy <sanusifatah16@gmail.com>
2024-11-21 13:45:09 +0100

    mnc

--- test/Escrow.ts
+++ test/Escrow.ts
@@ -31,9 +31,20 @@ describe("Escrow", () => {
       inspector.address,
       lender.address
     );
+
+    // apprived property
+    transaction = await realEstate.connect(seller).approve(escrow.address, 1);
+    await transaction.wait();
+
+    // list property
+    transaction = await escrow
+      .connect(seller)
+      .list(1, buyer.address, tokens(10), tokens(5));
+    await transaction.wait();
   });
 
-  it("save the addresses", async () => {});
+  // it("save the addresses", async () => {});
+
   describe("Deployment", () => {
     it("Return NFT address", async () => {
       const result = await escrow!.nftAddress();
@@ -50,6 +61,38 @@ describe("Escrow", () => {
     it("Return lender address", async () => {
       const result = await escrow!.lender();
       expect(result).to.be.equal(lender.address);
+    });
+  });
+
+  describe("Listing", () => {
+    it("Update as Listed", async () => {
+      const result = await escrow.isListed(1);
+      expect(result).to.be.equal(true);
+    });
+
+    it("Update Ownership", async () => {
+      expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
+    });
+
+    it("Returns buyer", async () => {
+      const result = await escrow.buyer(1);
+      expect(result).to.be.equal(buyer.address);
+    });
+    it("Returns Purchase price", async () => {
+      const result = await escrow.purchasePrice(1);
+      expect(result).to.be.equal(tokens(10));
+    });
+    it("Returns escrow amount", async () => {
+      const result = await escrow.escrowAmount(1);
+      expect(result).to.be.equal(tokens(5));
+    });
+
+    it("Log Ownership", async () => {
+      console.log(
+        `realEstateOwner: ${await realEstate.ownerOf(1)} & escrowAddress: ${
+          escrow.address
+        }`
+      );
     });
   });
 });
