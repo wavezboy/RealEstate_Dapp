48bf77fbdebf36e658d128e60053f9c451f3a67a
wavezboy <sanusifatah16@gmail.com>
2024-11-21 09:25:28 +0100

    mnc

--- test/Escrow.ts
+++ test/Escrow.ts
@@ -1,15 +1,17 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { RealEstate } from "../typechain-types";
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
 describe("Escrow", () => {
-  let buyer, seller, inspector, lender;
-  let realEstate, escrow;
+  let escrow;
 
-  it("save the addresses", async () => {
+  let realEstate: RealEstate;
+  let buyer, seller, inspector, lender;
+  beforeEach(async () => {
     [buyer, seller, inspector, lender] = await ethers.getSigners();
 
     // deply real estate
@@ -29,17 +31,25 @@ describe("Escrow", () => {
       inspector.address,
       lender.address
     );
+  });
 
-    let result = await escrow!.nftAddress();
-    expect(result).to.be.equal(realEstate.address);
-
-    result = await escrow!.seller();
-    expect(result).to.be.equal(seller.address);
-
-    result = await escrow!.inspector();
-    expect(result).to.be.equal(inspector.address);
-
-    result = await escrow!.lender();
-    expect(result).to.be.equal(lender.address);
+  it("save the addresses", async () => {});
+  describe("Deployment", () => {
+    it("Return NFT address", async () => {
+      const result = await escrow!.nftAddress();
+      expect(result).to.be.equal(realEstate.address);
+    });
+    it("Return seller address", async () => {
+      const result = await escrow!.seller();
+      expect(result).to.be.equal(seller.address);
+    });
+    it("Return inspector address", async () => {
+      const result = await escrow!.inspector();
+      expect(result).to.be.equal(inspector.address);
+    });
+    it("Return lender address", async () => {
+      const result = await escrow!.lender();
+      expect(result).to.be.equal(lender.address);
+    });
   });
 });
